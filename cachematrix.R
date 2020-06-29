aabe177f4623e208234eecfdcdd1a9a90d7d3ce0
Naganathan M <63549220+nagaopto@users.noreply.github.com>
2020-06-29 12:40:02 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,77 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function( m = matrix() ) {
+  
+## Inverse property initiation
+  
+  i <- NULL
+  set <- function( matrix ) {
+    m <<- matrix
+    i <<- NULL
+  }
+  
+  ## Get the matrix
+  
+  get <- function() {
+    ## Return the matrix
+    m
+  }
+  
+  ## Set the inverse of the matrix
+  setInverse <- function(inverse) {
+    i <<- inverse
+  }
+  
+  ## Get the inverse of the matrix
+  
+  getInverse <- function() {
+    ## Back the inverse property
+    i
+  }
+  
+  ## List of the methods
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
+
+## Inverse of the unique matrix back by makeCachematrix
+## Back to a matrix  “m”
+
+m <- x$getInverse()
+if( !is.null(m) ) {
+  message("getting cached data")
+  return(m)
+}
+
+## Inverse via matrix multiplication
+
+m <- solve(data) %*% data
+
+x$setInverse(m)
+
+m
+}
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  #get the value of the invertible matrix from the makeCacheMatrix function
+  invMatrix <- x$getInverse()
+  if(!is.null(invMatrix)) {                       #if inverse matrix is not NULL
+    message("Getting Cached Invertible Matrix")   #Type message: Getting Cached Invertible Matrix 
+    return(invMatrix)                             #return the invertible matrix
+  }
+  
+  #if value of the invertible matrix is NULL then  
+  MatrixData <- x$getMatrix()                     #get the original Matrix Data 
+  invMatrix <- solve(MatrixData, ...)             #use solve function to inverse the matrix
+  x$setInverse(invMatrix)                         #set the invertible matrix 
+  return(invMatrix)                               #return the invertible matrix
+  ## Return a matrix that is the inverse of 'x'
 }
+
